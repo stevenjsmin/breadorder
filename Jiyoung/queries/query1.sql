2b020d33304abc9183899c95e0314e8df18904b7
Steven Min <steven.jsmin@gmail.com>
2017-06-17 06:10:52 +1000

    Add order

--- Jiyoung/queries/query1.sql
+++ Jiyoung/queries/query1.sql
@@ -1,5 +1,14 @@
 select * from order_detail where order_date = '2017-06-17';
 
+select * from order_detail where orderer_name = '0423 391 66';
+
+
+update order_detail set orderer_name = '0423 391 661' where orderer_name = '0423 391 66';
+
+
+
+
+
 
 INSERT INTO order_detail (product_name, order_date, orderer_name, order_amount, orderer_contact_number, orderer_address, order_comment) VALUES
   ('고구마앙금빵',     '2017-06-17', '김영아', 4, null, '8 Callan Ct, Mill Park VIC 3082', '배달'),
@@ -18,3 +27,5 @@ INSERT INTO order_detail (product_name, order_date, orderer_name, order_amount, 
 INSERT INTO order_detail (product_name, order_date, orderer_name, order_amount, orderer_contact_number, orderer_address, order_comment) VALUES
   ('꽈베기',         '2017-06-17', '0400 098 009:두이엄마', 14, null, null, null),
   ('커피번',         '2017-06-17', '0400 098 009:두이엄마', 4, null, null, null);
+
+
